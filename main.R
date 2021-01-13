0ea62c981c3fc869527112066a8fb536d8f9d3ff
Agnaldo <agnaldosamuel3@mail.com>
2021-01-13 17:07:01 +0200

    exclui ficheiro da farmac para us que nao fazem referencia de pacientes

--- main.R
+++ main.R
@@ -1,41 +1,42 @@
-library(RMySQL)
-source('generic_functions.R')
+# 
 # install.packages("uuid")  -- Instalar este packaged primeiro
+
+library(RMySQL)
 library(uuid)
 
+##################### modificar os parametros
 
-#working dir
-wd <- '/home/agnaldo/Git/iDART/uuid_checker'
+#working dir       # ******** modificar
+wd <- '/home/agnaldo/Git/iDART/uuid_checker'   # ******** modificar
 setwd(wd)
+source('generic_functions.R')
 
-
+# nome da us 
+us_name <- 'polana_canico'                          # ******** modificar
+us_referencia_pacientes <- FALSE                    # ******** modificar (TRUE apenas para CS Xipamanine, porto, bagamoio,albazine ,chamanculo,altomae)
 ## OpenMRS  - Configuracao de variaveis de conexao 
-openmrs.user ='esaude'                     # ******** modificar
-openmrs.password='esaude'                  # ******** modificar
-openmrs.db.name='bagamoio'                 # ******** modificar
-openmrs.host='127.0.0.1'                   # ******** modificar
-openmrs.port=3306                          # ******** modificar
+openmrs.user ='esaude'                         # ******** modificar
+openmrs.password='esaude'                      # ******** modificar
+openmrs.db.name='polana_canico'                     # ******** modificar
+openmrs.host='127.0.0.1'                       # ******** modificar
+openmrs.port=3306                              # ******** modificar
 
-
-## iDART  - Configuracao de variaveis de conexao 
-postgres.user ='postgres'                      # ******** modificar
-postgres.password='iD@rt2020'                   # ******** modificar
-postgres.db.name='pharm'                  # ******** modificar
-postgres.host='192.168.1.163'                     # ******** modificar
-postgres.port=5432                             # ******** modificar
 
 # Objecto de connexao com a bd openmrs
 con_openmrs = dbConnect(MySQL(), user=openmrs.user, password=openmrs.password, dbname=openmrs.db.name, host=openmrs.host, port=openmrs.port)
+load(file = 'data/farmac_patients.RData')
+
+# Ignorar --> apenas para trazer pacientes da farmac
 #con_postgres <-  dbConnect(PostgreSQL(),user = postgres.user,password = postgres.password, dbname = postgres.db.name,host = postgres.host)
 #con.farmac <-  dbConnect(PostgreSQL(),user = postgres.user, password = postgres.password, dbname = postgres.db.name,host = postgres.host,port=postgres.port)
-
-load(file = 'data/farmac_patients.RData')
 #farmac_patients <- dbGetQuery( con.farmac , paste0("select * from public.sync_temp_patients ;" )  )
 #save( farmac_patients,file = 'data/farmac_patients.RData')
 
-## Pacientes
+
+
 ## Buscar todos pacientes OpenMRS 
 openmrs_patients <- getOpenMRSPatientsWrongUuid(con_openmrs)
+
 openmrs_patients$new_uuid <-""
 
 # para cada paciente gerar novo uuid
@@ -57,10 +58,15 @@ for (v in 1:nrow(openmrs_patients) ) {
   
   sql_update_idart_uuid <-  paste0("  update  public.patient set uuidopenmrs = '",
                                    new_uuid,"' , uuid = '",   new_uuid ,"' where uuidopenmrs = '",old_uuid, "' ;")
-  if(nid %in% farmac_patients$patientid){
-    write(sql_update_idart_uuid,file=sql_file_farmac,append=TRUE)
+  if(us_referencia_pacientes){
+    if(nid %in% farmac_patients$patientid){
+      
+      write(sql_update_idart_uuid,file=sql_file_farmac,append=TRUE)
+      
+    }
     
   }
+
 
 #write("-- ------------------------------------------------------------------------------------------------",file=sql_file_idart,append=TRUE)
 write(sql_update_idart_uuid,file=sql_file_idart,append=TRUE)
